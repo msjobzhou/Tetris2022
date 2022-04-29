81ed56d8304331c41a3e38bb70a64040dfc4be2d
msjobzhou <msjobzhou@hotmail.com>
2022-04-29 23:05:35 +0800

    增加RunTetrisGameSimulation等函数

--- PDTetrisControllerUsingGeneticAlgorithm.cpp
+++ PDTetrisControllerUsingGeneticAlgorithm.cpp
@@ -10,3 +10,16 @@ CPDTetrisControllerUsingGeneticAlgorithm::CPDTetrisControllerUsingGeneticAlgorit
 CPDTetrisControllerUsingGeneticAlgorithm::~CPDTetrisControllerUsingGeneticAlgorithm()
 {
 }
+//针对当前CPierreDellacherieTetrisController模拟运行俄罗斯方块游戏，并返回其对应的fitness值
+void CPDTetrisControllerUsingGeneticAlgorithm::RunTetrisGameSimulation(CPierreDellacherieTetrisController &pdController, double &dFitness)
+{
+	//用于训练的俄罗斯方块序列为空时，不做任何动作
+	if (m_vecTetrisBlockType.empty())
+		return;
+
+	for (vector<int>::iterator it = m_vecTetrisBlockType.begin(); it != m_vecTetrisBlockType.end(); it++)
+	{
+		int nBlockType = *it;
+
+	}
+}
--- PDTetrisControllerUsingGeneticAlgorithm.h
+++ PDTetrisControllerUsingGeneticAlgorithm.h
@@ -12,13 +12,24 @@ class CPDTetrisControllerUsingGeneticAlgorithm
 {
 private:
 	//storage for the population of genomes
-	//每一代人口的基因，对应着每一个PDTetrisControllers的特征系数(权重)
+	//每一代人口的基因，每一个个体基因对应着每一个PDTetrisControllers的特征系数(权重)
 	vector<CGenome>	     m_vecPopulation;
 	//m_vecPDTetrisControllers和m_vecPopulation一一对应
 	vector<CPierreDellacherieTetrisController> m_vecPDTetrisControllers;
 
+	//用于训练的俄罗斯方块序列，所有的个体都使用这一个序列进行训练
+	//具体的序列取值在构造函数中随机生成，数量由m_nTetrisBlockNum进行控制
+	vector<int> m_vecTetrisBlockType;
+	int m_nTetrisBlockNum;
+	//记录遗传算法已经进行到第多少代
+	int	m_nCurrentGeneration;
+
+	//指向遗传算法的指针
+	CGeneticAlgrithom* m_pGA;
 public:
 	CPDTetrisControllerUsingGeneticAlgorithm();
 	~CPDTetrisControllerUsingGeneticAlgorithm();
+	
+	void RunTetrisGameSimulation(CPierreDellacherieTetrisController &pdController, double& dFitness);
 };
 
